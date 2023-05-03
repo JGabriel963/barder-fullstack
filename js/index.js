ad5fc8a6efd751c6caf03ac4db3d9b6be9f083d4
João Gabriel <112815316+JGabriel963@users.noreply.github.com>
2023-05-03 16:41:03 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,61 @@
+// validation
+const inputFields = {
+    code: document.getElementById('code'),
+    data: document.getElementById('date'),
+    amount: document.getElementById('amount'),
+    unitaryValue: document.getElementById('unitary-value'),
+    brokerageFee: document.getElementById('brokerage-fee')
+}
+
+function validateEmail(email) {
+    if (!email.match(/[A-Z]{4}\d{1,2}/)) {
+        const err = new Error("Código inválido")
+        err.input = 'code'
+        throw err
+    }
+}
+
+function validateDate(date) {
+    if (!date.match(/\d{2}\/\d{2}\/\d{4}/)) {
+        const err = new Error("Data inválida")
+        err.input = 'data'
+        throw err
+    }
+}
+
+function validateAmout(amount) {
+    if (!amount.match(/^\d+$/)) {
+        const err = new Error("Valor inválido.")
+        err.input = 'amount'
+        throw err
+    }
+}
+
+function validateUnitary(value) {
+    if (!value.match(/^[0-9]+([,][0-9]+?$)/)) {
+        const err = new Error("Valor inválido.")
+        err.input = 'unitaryValue'
+        throw err
+    }
+}
+
+function validateBrokerageFee(value) {
+    if (!value.match(/^\d+$/)) {
+        const err = new Error("Valor inválido.")
+        err.input = 'brokerageFee'
+        throw err
+    }
+}
+
+function validateFields() {
+    validateEmail(inputFields.code.value)
+    validateDate(inputFields.data.value)
+    validateAmout(inputFields.amount.value)
+    validateUnitary(inputFields.unitaryValue.value)
+    validateBrokerageFee(inputFields.brokerageFee.value)
+}
+// End
+
 // Inicio do TEste
 const openModal = () => document.getElementById('modal').classList.add('active')
 
@@ -5,23 +63,60 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active')
 }
 
+const alertError = (error) => {
+    switch (error) {
+        case 'code':
+            alert("Código inválido!")
+            break
+        case 'data':
+            alert('Data inválida! Padrão - DD/MM/AAAA')
+            break
+        case 'amount':
+            alert("Quantidade inválida")
+            break
+        case 'unitaryValue':
+            alert('Valor unitário inválido.')
+            break
+        case 'brokerageFee':
+            alert("Taxa corretária inválida.")
+            break
+        default:
+            alert("Todos estão inválidos.")
+            break
+    }
+}
+
+function valorFinal(valor, valorop, taxa, imposto) {
+    if (valor === "C") {
+        return valorop + taxa + imposto
+    } else {
+        return valorop - taxa - imposto
+    }
+}
+
 const saveEditInvestiment = () => {
-    if(isValidFields()) {
-        const investment = {
-            code: document.getElementById('code').value,
-            data: document.getElementById('date').value,
-            amount: document.getElementById('amount').value,
-            unitaryValue: document.getElementById('unitary-value').value,
-            buySell: document.getElementById('buy-sell').value,
-            brokerageFee: document.getElementById('brokerage-fee').value,
-            opValue: document.getElementById('amount').value * document.getElementById('unitary-value').value,
-            imposto: (((document.getElementById('amount').value * document.getElementById('unitary-value').value) * 0.03) / 100).toFixed(2),
-            finalValue: (document.getElementById('amount').value * document.getElementById('unitary-value').value) + 1.92
+    try {
+        validateFields()
+        if (isValidFields()) {
+            const investment = {
+                code: document.getElementById('code').value,
+                data: document.getElementById('date').value,
+                amount: document.getElementById('amount').value,
+                unitaryValue: document.getElementById('unitary-value').value,
+                buySell: document.getElementById('buy-sell').value,
+                brokerageFee: document.getElementById('brokerage-fee').value,
+                opValue: document.getElementById('amount').value * (document.getElementById('unitary-value').value).replace(',', '.'),
+                imposto: (((document.getElementById('amount').value * (document.getElementById('unitary-value').value).replace(',', '.')) * 0.03) / 100).toFixed(2),
+                finalValue: valorFinal(document.getElementById('buy-sell').value, document.getElementById('amount').value * (document.getElementById('unitary-value').value).replace(',', '.'), document.getElementById('brokerage-fee').value, (((document.getElementById('amount').value * (document.getElementById('unitary-value').value).replace(',', '.')) * 0.03) / 100).toFixed(2))
+            }
+            const index = document.getElementById('code').dataset.index
+            updateClient(index, investment)
+            updateTable()
+            closeModal()
         }
-        const index = document.getElementById('code').dataset.index
-        updateClient(index, investment)
-        updateTable()
-        closeModal()
+    } catch (err) {
+        inputFields[err.input].classList.add('error')
+        alertError(err.input)
     }
 }
 // Fim do teste
